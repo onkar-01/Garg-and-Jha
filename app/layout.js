99b973af35ec2d9794c60e26f4e35d47d8847280
Onkar Vatsa <onkarvatsa@gmail.com>
2023-08-29 09:31:23 +0530

    ..

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./styles/globals.scss";
 import "animate.css";
 import { Inter } from "next/font/google";
 import Footer from "./components/Footer";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,14 +16,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
-        />
+        <Head>
+          <title>{metadata.title}</title>
+          <meta name="description" content={metadata.description} />
+          <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+          />
+        </Head>
+
         <Header />
         {children}
         <Footer />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js"></script>
       </body>
     </html>
   );
